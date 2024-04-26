f2b817b96a3f6149601b3f1c1f6dec1ae8bf7824
Alexey Romanenko <alexey.v.romanenko@gmail.com>
2024-04-26 15:54:51 +0300

    =check1

--- test/test.cpp
+++ test/test.cpp
@@ -1,20 +1,10 @@
-/*#define BOOST_TEST_MODULE hw-1_ut
-
-#include 
-
-#include <lib.h>
-
-BOOST_AUTO_TEST_SUITE(hw-1_ut)
-
-BOOST_AUTO_TEST_CASE(test)
-{
-    //lib::
-}*/
 
 #include <iostream>
+#include <lib.h>
 
 int main(int argc, char const *argv[])
 {
+    lib::print_hello_world();
     std::cout << "tests passed OK:" << std::endl;
     return 0;
 }
