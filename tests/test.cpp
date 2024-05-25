2c31bb22e0ed165215d74ae1d823811c6094c0eb
Alexey Romanenko <alexey.v.romanenko@gmail.com>
2024-05-25 14:39:22 +0300

    1

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,6 +5,8 @@ BOOST_AUTO_TEST_SUITE(TESTING)
 
 BOOST_AUTO_TEST_CASE(Test_1)
 {
+	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_level::log_messages);
+
 	BOOST_TEST_MESSAGE("Tests OK.");
 }
 
