ea98b738d2ba2600c70bbbf1e5ef478a8198bfef
Alexey Romanenko <alexey.v.romanenko@gmail.com>
2024-05-25 21:59:07 +0300

    1

--- my_allocator/my_allocator.h
+++ my_allocator/my_allocator.h
@@ -2,39 +2,72 @@
 
 namespace my
 {
-	template <class T, unsigned int PREDEF_N>
-	struct allocator
+	struct deleter
+	{
+		void operator()(void* ptr)
+		{
+			::operator delete(ptr);
+		}
+	};
+
+	template <class T>
+	struct allocator_10
 	{
 		using value_type = T;
 
-		allocator() noexcept
+		static const uint32_t PRELOC_N = 10;
+
+		allocator_10() noexcept :
+			m_pool(::operator new (sizeof(uint8_t)* PRELOC_N), deleter()),
+			m_pool_elems(PRELOC_N)
 		{
 		}
 
 		template <class U>
-		allocator(const allocator<U>&) noexcept
+		allocator_10(const allocator_10<U>& a) noexcept
 		{
+			m_pool = a.m_pool;
+			m_pool_elems = a.m_pool_elems;
 		}
 
 		T* allocate(std::size_t n)
 		{
+			if ((m_user_elems + n) > m_pool_elems)
+			{
+				throw std::bad_alloc();
+				return nullptr;
+			}
+
+			m_user_elems += n;
+
 			return static_cast<T*>(::operator new(n * sizeof(T)));
 		}
 
 		void deallocate(T* p, std::size_t n)
 		{
-			::operator delete(p);
+			
 		}
+
+		//initial pool
+		std::shared_ptr<void> m_pool;
+		std::size_t m_pool_elems = 0;
+
+		//user
+		std::size_t m_user_elems = -1; //1 - undefined map constructor element
+
+		/*using propagate_on_container_copy_assignment = std::true_type;
+		using propagate_on_container_move_assignment = std::true_type;
+		using propagate_on_container_swap = std::true_type;*/
 	};
 
 	template <class T, class U>
-	constexpr bool operator== (const allocator<T>& a1, const allocator<U>& a2) noexcept
+	constexpr bool operator== (const allocator_10<T>& a1, const allocator_10<U>& a2) noexcept
 	{
 		return true;
 	}
 
-	template <class T, class U>
-	constexpr bool operator!= (const allocator<T>& a1, const allocator<U>& a2) noexcept
+	template <class T, class U, unsigned int PREDEF_N>
+	constexpr bool operator!= (const allocator_10<T>& a1, const allocator_10<U>& a2) noexcept
 	{
 		return false;
 	}
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,37 @@
 #define BOOST_TEST_MODULE test
 #include <boost/test/unit_test.hpp>
 
+#include <map>
+#include <string>
+
+#include <my_allocator.h>
+
 BOOST_AUTO_TEST_SUITE(TESTING)
 
 BOOST_AUTO_TEST_CASE(Test_1)
 {
 	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_level::log_messages);
+
+	{
+		using int_str_map = std::map<int, std::string>;
+		using int_str_map_my_alloc = std::map<int, std::string, std::less<int_str_map::key_type>, my::allocator_10<int_str_map::value_type>>;
+
+		int_str_map_my_alloc my_map;
+
+		my_map = { { 1, "1" }, { 2, "2" }, { 3, "3" }, { 4, "4" }, { 5, "5" } };
+
+		for (int i = 6; i < 12; ++i)
+		{
+			if (i == 11)
+			{
+				BOOST_CHECK_THROW(my_map.insert({ i, std::to_string(i) }), std::bad_alloc);
+			}
+			else
+			{
+				BOOST_CHECK_NO_THROW(my_map.insert({ i, std::to_string(i) }));
+			}			
+		}
+	}
 
 	BOOST_TEST_MESSAGE("Tests OK.");
 }
