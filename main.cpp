85e180fe3d6ca8cfc6a4200c15aca71736e5cbcf
Alexey Romanenko <alexey.v.romanenko@gmail.com>
2024-05-30 21:18:19 +0300

    1409

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 int main(int, char**)
 {
+	int garry = 0, larry = 0;
+	std::cin >> garry;
+	std::cin >> larry;
+
+	int total = garry + larry;
+	if (total > 10)
+		total = 10;
+	else
+		total = total - 1;
+
+	std::cout << (total - garry) << " " << (total - larry);
 
 	return 0;
 }
