16961b2218873d6b9da2b8206250afbc99c22563
Alexey Romanenko <alexey.v.romanenko@gmail.com>
2024-06-03 23:43:43 +0300

    v0.1

--- my_lib/my_traits.h
+++ my_lib/my_traits.h
@@ -14,4 +14,19 @@ namespace my
 		}
 		std::cout << (int)arr[0] << std::endl;
 	};
+
+	//template<class String, typename std::enable_if_t<std::is_same_v<String, std::string>, bool> = true>
+	template<class String, typename std::enable_if_t<std::is_convertible_v<String, std::string>, bool> = true>
+	void print_ip(const String& val)
+	{
+		std::cout << val << std::endl;
+	}
+
+	template <class T, template <class...> class Template>
+	struct is_specialization_of : std::false_type {};
+
+	template <template <class...> class Template, class... Args>
+	struct is_specialization_of<Template<Args...>, Template> : std::true_type {};
+
+	constexpr bool b = is_specialization_of<std::tuple<int, double, std::string>, std::tuple>::value;
 }
