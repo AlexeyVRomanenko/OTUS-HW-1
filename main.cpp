5cc2120f28912d37601dac12d787bb0b7e2caef0
Alexey Romanenko <alexey.v.romanenko@gmail.com>
2024-08-04 23:21:50 +0300

    v1.0

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <ffinder/ffinder.h>
 
+//#include <thread>
+//#include <chrono>
+
 int main(int argc, char** cmd_line)
 {
+	//std::this_thread::sleep_for(std::chrono::seconds(5));
+
 	auto finder = ffinder::FilesDuplicateFinder::Instance(cmd_line[1]);
 	if (!finder)
 		return 1;
